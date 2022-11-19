86b82fe9a9603a7455a4a790d138eb28503566d8
andrewvitrenko <avitrenko2004@gmail.com>
2022-11-19 16:41:19 +0100

    feat(todos): implemented add, edit, delete todo

--- controls/index.js
+++ controls/index.js
@@ -80,7 +80,7 @@ router.post('/signup', async (req, res) => {
   }
 });
 
-router.get('/getTodos', authMiddleware, async (req, res) => {
+router.get('/todos', authMiddleware, async (req, res) => {
   try {
     const { userId } = req.user;
     const user = await Users.findById(userId);
@@ -96,6 +96,147 @@ router.get('/getTodos', authMiddleware, async (req, res) => {
       todos: user.tasks,
     });
   } catch (e) {
+    res.status(500).json({
+      message: ERRORS.INTERNAL_SERVER_ERROR,
+    });
+  }
+});
+
+router.post('/todo', authMiddleware, async (req, res) => {
+  try {
+    const { userId } = req.user;
+    const { text, startTime, endTime, background } = req.body;
+
+    if (!text) {
+      res.status(400).json({
+        message: ERRORS.TODO_TEXT_REQUIRED,
+      });
+      return;
+    }
+
+    if (!startTime) {
+      res.status(400).json({
+        message: ERRORS.TODO_START_REQUIRED,
+      });
+      return;
+    }
+
+    if (!endTime) {
+      res.status(400).json({
+        message: ERRORS.TODO_END_REQUIRED,
+      });
+      return;
+    }
+
+    const user = await Users.findById(userId);
+
+    if (!user) {
+      res.status(404).json({
+        message: ERRORS.USER_NOT_FOUND,
+      });
+      return;
+    }
+
+    const id = crypto.randomBytes(10).toString('hex');
+    const finalTodo = {
+      _id: id,
+      startTime,
+      endTime,
+      text,
+      background: background || null,
+    };
+
+    user.tasks.push(finalTodo);
+    await user.save();
+
+    res.status(201).json({
+      todo: finalTodo,
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: ERRORS.INTERNAL_SERVER_ERROR,
+    });
+  }
+});
+
+router.patch('/todo/:todoId', authMiddleware, async (req, res) => {
+  try {
+    const { userId } = req.user;
+    const { todoId } = req.params;
+    const todo = req.body;
+
+    if (!todo) {
+      res.status(400).json({
+        message: ERRORS.TODO_REQUIRED,
+      });
+      return;
+    }
+
+    const user = await Users.findById(userId);
+
+    if (!user) {
+      res.status(404).json({
+        message: ERRORS.USER_NOT_FOUND,
+      });
+      return;
+    }
+
+    const todoIndex = user.tasks.findIndex(task => task._id === todoId);
+
+    if (todoIndex === -1) {
+      res.status(404).json({
+        message: ERRORS.TODO_NOT_FOUND,
+      });
+      return;
+    }
+
+    user.tasks[todoIndex] = {
+      ...user.tasks[todoIndex]._doc,
+      ...todo,
+    };
+
+    await user.save();
+    res.status(200).json({
+      todo: user.tasks[todoIndex],
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: ERRORS.INTERNAL_SERVER_ERROR,
+    });
+  }
+});
+
+router.delete('/todo/:todoId', authMiddleware, async (req, res) => {
+  try {
+    const { userId } = req.user;
+    const { todoId } = req.params;
+
+    const user = await Users.findById(userId);
+
+    if (!user) {
+      res.status(404).json({
+        message: ERRORS.USER_NOT_FOUND,
+      });
+      return;
+    }
+
+    const todo = user.tasks.find(task => task._id === todoId);
+
+    if (!todo) {
+      res.status(404).json({
+        message: ERRORS.TODO_NOT_FOUND,
+      });
+      return;
+    }
+
+    user.tasks = user.tasks.filter(task => task._id !== todoId);
+
+    await user.save();
+    res.status(200).json({
+      message: 'Todo was successfully deleted',
+    });
+  } catch (e) {
+    console.log(e);
     res.status(500).json({
       message: ERRORS.INTERNAL_SERVER_ERROR,
     });
